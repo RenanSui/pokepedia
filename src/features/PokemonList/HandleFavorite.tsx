4f59202ce1be4b6e86dba9a8cf4375e395a0dcee
RenanSui <renanddtao@gmail.com>
2023-04-20 09:20:47 -0300

    HandleFavorite's add and remove favorite functions abstracted

--- src/features/PokemonList/HandleFavorite.tsx
+++ src/features/PokemonList/HandleFavorite.tsx
@@ -8,24 +8,10 @@ export const HandleFavorite = (name: string, id: number) => {
 	const FavoriteIds = PokedexFavorites.map((Favorite) => Favorite.id);
 
 	// ? Remove Favorite
-	if (FavoriteIds.includes(id)) {
-		const FavoriteIndex = PokedexFavorites.findIndex(
-			(Favorite) => Favorite.id === id
-		);
-		const newFavorite = [
-			...PokedexFavorites.slice(0, FavoriteIndex),
-			...PokedexFavorites.slice(FavoriteIndex + 1),
-		];
-		localStorage.setItem('PokedexFavorites', JSON.stringify(newFavorite));
-		return null;
-	}
+	if (FavoriteIds.includes(id)) removeFavorite(PokedexFavorites, id);
 
 	// ? Add Favorite
-	if (!FavoriteIds.includes(id)) {
-		const AddFavorite = JSON.stringify([...PokedexFavorites, { id, name }]);
-		localStorage.setItem('PokedexFavorites', AddFavorite);
-		return null;
-	}
+	if (!FavoriteIds.includes(id)) addFavorite(PokedexFavorites, id, name);
 };
 
 export const getLocalStorage = (): Favorites[] => {
@@ -35,4 +21,24 @@ export const getLocalStorage = (): Favorites[] => {
 	return localStorage.getItem('PokedexFavorites')
 		? JSON.parse(PokedexFavorites)
 		: [];
+};
+
+export const removeFavorite = (PokedexFavorites: Favorites[], id: number) => {
+	const FavoriteIndex = PokedexFavorites.findIndex(
+		(Favorite) => Favorite.id === id
+	);
+	const newFavorite = [
+		...PokedexFavorites.slice(0, FavoriteIndex),
+		...PokedexFavorites.slice(FavoriteIndex + 1),
+	];
+	localStorage.setItem('PokedexFavorites', JSON.stringify(newFavorite));
+};
+
+export const addFavorite = (
+	PokedexFavorites: Favorites[],
+	id: number,
+	name: string
+) => {
+	const AddFavorite = JSON.stringify([...PokedexFavorites, { id, name }]);
+	localStorage.setItem('PokedexFavorites', AddFavorite);
 };

29baa7320320fe09963ae5ec804a534372033c52
RenanSui <renanddtao@gmail.com>
2024-07-16 19:07:19 -0300

    create pokemons page and layout

---
+++ src/app/pokemons/layout.tsx
@@ -0,0 +1,18 @@
+import { SiteFooter } from '@/components/layouts/site-footer'
+import { SiteHeader } from '@/components/layouts/site-header'
+import * as React from 'react'
+
+export default function PokemonsLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="relative flex min-h-[calc(100vh)] flex-col">
+      {/* <div className="grainy-noise absolute left-1/2 top-1/2 min-h-screen w-full -translate-x-1/2 -translate-y-1/2" /> */}
+      <SiteHeader />
+      <main className="flex-1">{children}</main>
+      <SiteFooter />
+    </div>
+  )
+}
---
+++ src/app/pokemons/page.tsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <div className="relative min-h-[calc(100vh-154px)]">Olá</div>
+}

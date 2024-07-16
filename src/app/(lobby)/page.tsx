638b28748887a199e3609e01647905e97614ddaf
RenanSui <renanddtao@gmail.com>
2024-07-16 19:06:57 -0300

    modify lobby page

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,19 +1,3 @@
-import { PokemonList } from '@/components/pokemon-list'
-
-const Home = () => {
-  return (
-    <>
-      <header></header>
-      <main>
-        <PokemonList />
-      </main>
-      <footer></footer>
-    </>
-  )
+export default function LobbyPage() {
+  return <div>LobbyPage</div>
 }
-
-export default Home
-
-// bg-[#1F2628] bg color
-// border-[#42443A] yellow
-// border-[#304358] blue

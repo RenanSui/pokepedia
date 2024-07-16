a8f69507b6f68afe9f30d2eb6629818c357ee675
RenanSui <renanddtao@gmail.com>
2024-07-16 19:24:09 -0300

    create vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// import react from '@vitejs/plugin-react';
+// import { loadEnv } from 'vite';
+// import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['src/**/*.test.{js,jsx,ts,tsx}'],
+    coverage: {
+      include: ['src/**/*'],
+      exclude: ['src/**/*.stories.{js,jsx,ts,tsx}', '**/*.d.ts'],
+      reporter: ['html'],
+    },
+  },
+})

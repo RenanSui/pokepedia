03363c54cc25bf6168e070d031496052a2af1125
RenanSui <renanddtao@gmail.com>
2024-07-16 19:02:44 -0300

    create providers

---
+++ src/components/providers/providers.tsx
@@ -0,0 +1,7 @@
+import { NextThemesProvider } from './theme-provider'
+
+export const Providers = async ({
+  children,
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  return <NextThemesProvider>{children}</NextThemesProvider>
+}

b05d61f60d1bd70670e3ba6d6121eb71d45d1d22
RenanSui <renanddtao@gmail.com>
2024-07-16 19:02:31 -0300

    create site config

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+const links = {
+  x: 'https://twitter.com/adsjksui',
+  github: 'https://github.com/RenanSui/pokepedia',
+  githubAccount: 'https://github.com/RenanSui',
+  discord: 'https://discord.com/users/seyonsui',
+}
+
+export const siteConfig = {
+  name: 'Poképedia',
+  links,
+}

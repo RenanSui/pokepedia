04a1cf3afdabe32ca21979478710f5d1a76c15a1
RenanSui <renanddtao@gmail.com>
2024-07-18 17:55:00 -0300

    create pokemon pagination size

---
+++ src/app/pokemons/_components/pokemon-pagination-size.tsx
@@ -0,0 +1,42 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { useConfigPaginationSize } from '@/hooks/use-pokemon-atom'
+import { cn } from '@/lib/utils'
+
+export function PokemonPaginationSize({
+  pageSizeOptions = [10, 20, 30, 40, 50],
+  className,
+}: {
+  pageSizeOptions?: number[]
+  className?: string
+}) {
+  const [configSize, setConfigSize] = useConfigPaginationSize()
+
+  return (
+    <div className={cn('flex items-center space-x-2', className)}>
+      <p className="whitespace-nowrap text-sm font-medium">Rows per page</p>
+      <Select
+        value={`${configSize}`}
+        onValueChange={(value) => {
+          setConfigSize(Number(value))
+        }}
+      >
+        <SelectTrigger className="h-8 w-[70px]">
+          <SelectValue placeholder={configSize} />
+        </SelectTrigger>
+        <SelectContent side="top">
+          {pageSizeOptions.map((pageSize) => (
+            <SelectItem key={pageSize} value={`${pageSize}`}>
+              {pageSize}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}

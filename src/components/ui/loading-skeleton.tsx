ff9fc292f53997d2b5668c263706b5e6d15a0ade
RenanSui <renanddtao@gmail.com>
2024-07-18 16:26:27 -0300

    remove unused components

--- src/components/ui/loading-skeleton.tsx
+++
@@ -1,7 +0,0 @@
-const LoadingSkeleton = () => {
-  return (
-    <span className="absolute -left-20 -translate-x-1/2 -rotate-90 animate-loading  bg-[linear-gradient(0deg,_rgba(0,0,0,0)_0%,_rgba(63,63,70,1)_50%,_rgba(0,0,0,0)_100%)] px-52 py-32" />
-  )
-}
-
-export { LoadingSkeleton }

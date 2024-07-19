532d8994eb6ad6ce07c4979b939b01db14c44e32
RenanSui <renanddtao@gmail.com>
2024-07-19 19:29:34 -0300

    modify site header

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,11 +1,13 @@
+import { siteConfig } from '@/config/site'
 import { MainNav } from './main-nav'
+import { MobileNav } from './mobile-nav'
 
 export const SiteHeader = async () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex items-center">
-        <MainNav />
-        {/* <MobileNav items={siteConfig.mainNav} /> */}
+        <MainNav items={siteConfig.mainNav} />
+        <MobileNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2"></nav>
         </div>

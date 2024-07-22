00315897f03911c7dd8518bb6331692d32780f19
RenanSui <renanddtao@gmail.com>
2024-07-22 16:55:54 -0300

    modify pokemon page

--- src/app/pokemons/page.tsx
+++ src/app/pokemons/page.tsx
@@ -1,5 +1,17 @@
+import { cookies } from 'next/headers'
+import { PokemonLayout } from './_components/pokemon-layout'
 import { PokemonPage } from './_components/pokemon-page'
 
 export default function page() {
-  return <PokemonPage />
+  const layout = cookies().get('react-resizable-panels:layout')
+
+  const defaultLayout = (layout ? JSON.parse(layout.value) : undefined) as
+    | number[]
+    | undefined
+
+  return (
+    <PokemonLayout defaultLayout={defaultLayout} sidePanel={<div>Oi</div>}>
+      <PokemonPage />
+    </PokemonLayout>
+  )
 }
